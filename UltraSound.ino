d4b84f3bccda3e54e7dd436c726dbef5c63ba774
MainFrameKuznetSov <121382196+MainFrameKuznetSov@users.noreply.github.com>
2023-01-03 22:04:05 +0530

    Update UltraSound.ino

--- UltraSound.ino
+++ UltraSound.ino
@@ -1,11 +1,11 @@
 #include <NewPing.h>
 #include <New_Ping.h>
-#define TPin 12
-#define EPin 13
+#define TPin 12// initialising the TRIGGER PIN with 12 of Arduino Digital Pin
+#define EPin 13// initialising the ECHO PIN with 12 of Arduino Digital Pin
 #define MaxDist 500
-NewPing np(TPin,EPin,MaxDist);
+NewPing np(TPin,EPin,MaxDist);// Creating NewPing object 'np'
 float dur,dist;
-const int iter=5;
+const int iter=5;// Fixing no. of pulses as 5
 void setup() 
 {
   // put your setup code here, to run once:
@@ -15,9 +15,9 @@ void setup()
 void loop() 
 { 
   // put your main code here, to run repeatedly:
-  dur=np.ping_median(iter);
-  dist=(dur/2)*0.0343;
-  if(dist>500 || dist<1)
+  dur=np.ping_median(iter);// Calculates the median of the 5(here) duration pulses 
+  dist=(dur/2)*0.0343;// Assuming velocity of sound to be 343 m/s
+  if(dist>500 || dist<1)// Setting limit of the HC-SR04 b/w 1 cm and 500 cm
   {
     Serial.println("Not in Range");
   }
